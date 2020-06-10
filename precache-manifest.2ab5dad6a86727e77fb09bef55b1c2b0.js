d2b54bf2024c8485aebcceb0b3448c89f1879349
4rsalan <arsalan.farooqui2@georgebrown.ca>
2020-06-10 19:27:31 -0400

    Updates

---
+++ precache-manifest.2ab5dad6a86727e77fb09bef55b1c2b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "476b73935218f440c3ebbfd83beb6504",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f53f193fbb3d7c6c7450",
+    "url": "/portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "08a616bc7b876c1a843f",
+    "url": "/portfolio/static/js/2.eae9afa0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.eae9afa0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f53f193fbb3d7c6c7450",
+    "url": "/portfolio/static/js/main.df9b0570.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "b39e5967936d894cceb7e6aea862fc31",
+    "url": "/portfolio/static/media/me.b39e5967.jpg"
+  }
+]);
