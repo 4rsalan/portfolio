211cb6b648b9143f24ca71d6921eed9646ff78c9
4rsalan <arsalan.farooqui2@georgebrown.ca>
2020-06-10 20:52:07 -0400

    Added Routing

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.2ab5dad6a86727e77fb09bef55b1c2b0.js"
+  "/portfolio/precache-manifest.8783b02504469e46bb5b8b3137d17ee6.js"
 );
 
 self.addEventListener('message', (event) => {
